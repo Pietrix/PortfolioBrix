a94376f806346f1961142ca6c4c6a82c41f0623f
PieterVnunen <p.vannunen@student.fontys.nl>
2024-06-17 22:34:43 +0200

    iamges

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env:{GRAPHQL_ENDPOINT: "http://portfolio.pieterswebsite.nl/graphql"}
+    env:{GRAPHQL_ENDPOINT: "http://portfolio.pieterswebsite.nl/graphql"},
+    images: { unoptimized: true, }
 };
 
 export default nextConfig;
